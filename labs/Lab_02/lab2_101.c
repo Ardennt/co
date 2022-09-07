258f317422c06d98faaa97f4f4e8ada9a4a63974
Ardennt <jiaweiw02@gmail.com>
2022-09-07 00:13:56 -0400

    lab2_

---
+++ labs/Lab_02/lab2_101.c
@@ -0,0 +1,29 @@
+#include <stdbool.h>                        // needed for bool type
+
+#include <stdio.h>
+
+int main() {
+   int nums[50];
+   int count;
+
+   // Step 0: Input values
+   scanf("%d", &count);
+   for (int i = 0; i < count; ++i) {
+      scanf("%d", &nums[i]);
+   }
+
+   // Step 1: Find minimum and maximum values
+   /* Type your code here. */
+
+   // Step 2: Calculate mean
+   /* Type your code here. */
+
+   // Step 3: Check if palindrome
+   /* Type your code here. */
+
+   // Step 4: Read the same array elements in an ascending order. Find and output median
+   /* Type your code here. */
+
+   // Step 5: Find and output mode of the sorted array
+   /* Type your code here. */
+}
