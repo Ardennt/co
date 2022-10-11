a571b943886c4917f3cde7082015346d4f888c33
Ardennt <jiaweiw02@gmail.com>
2022-10-10 20:11:58 -0400

    hw3 reference error

--- homework/Homework_03/hw03.s
+++ homework/Homework_03/hw03.s
@@ -46,33 +46,33 @@ main:
   move  $a2, $s2
   jal mm_print
 
-  # # Read in matrix 2 of some NxM size
-  # sub $sp, $sp, 4   # make space on stack for return value
-  # jal mm_read
+  # Read in matrix 2 of some NxM size
+  sub $sp, $sp, 4   # make space on stack for return value
+  jal mm_read
 
-  # # save matrix 2 values
-  # move  $s3, $v0
-  # move  $s4, $v1
-  # lw    $s5, 0($sp)
-  # add   $sp, $sp, 4   # restore stack
+  # save matrix 2 values
+  move  $s3, $v0
+  move  $s4, $v1
+  lw    $s5, 0($sp)
+  add   $sp, $sp, 4   # restore stack
 
-  # # Setup arguments and print out matrix 2
-  # move  $a0, $s3
-  # move  $a1, $s4
-  # move  $a2, $s5
-  # jal mm_print
+  # Setup arguments and print out matrix 2
+  move  $a0, $s3
+  move  $a1, $s4
+  move  $a2, $s5
+  jal mm_print
 
-  # # Setup arguments and call mm_mult
-  # # Note: 5 arguments, so we need to use stack
-  # # Note: num cols matrix 1 == num rows matrix 2
-  # move  $a0, $s0    # num rows matrix1
-  # move  $a1, $s1    # num cols matrix1
-  # move  $a2, $s2    # address of matrix 1
-  # move  $a3, $s4    # num cols matrix2
-  # sub   $sp, $sp, 4   
-  # sw    $s5, 0($sp) # address of matrix 2
-  # jal mm_mult
-  # add   $sp, $sp, 4
+  # Setup arguments and call mm_mult
+  # Note: 5 arguments, so we need to use stack
+  # Note: num cols matrix 1 == num rows matrix 2
+  move  $a0, $s0    # num rows matrix1
+  move  $a1, $s1    # num cols matrix1
+  move  $a2, $s2    # address of matrix 1
+  move  $a3, $s4    # num cols matrix2
+  sub   $sp, $sp, 4   
+  sw    $s5, 0($sp) # address of matrix 2
+  jal mm_mult
+  add   $sp, $sp, 4
 
   # # print the result
   # move $a0, $s0 
@@ -83,6 +83,8 @@ main:
   # restore $ra, free stack space, and return
   lw  $ra, 0($sp)
   add $sp, $sp, 4
+  li $v0, 10
+  syscall
   jr  $ra
 
 ################################################################################
@@ -120,11 +122,11 @@ mm_read:
   move $a1, $s2
   jal mm_alloc # return address will change here
   move $ra, $s3
-  move $sp, $v0
-  # sw $v0, 0($sp) # base address of matrix
   
   # do nested loops to read in values
-  move $t0, $sp # save "memory address" in t0
+  sw $v0, 0($sp) # save "memory address" in t0
+  lw $t0, 0($sp) # t0 will point at the address of $sp
+
   move $t1, $zero # int i = 0
   For1:
   	move $t2, $zero # int j = 0
@@ -133,9 +135,9 @@ mm_read:
       syscall # read in integer
   		sw $v0, 0($t0) # save the integer into the address
 
-      li $v0, 1
-      move $a0, $t0
-      syscall # print out address
+      # li $v0, 1
+      # move $a0, $t0
+      # syscall # print out address
     
   		addi $t2, $t2, 1 # int j += 1
       addi $t0, $t0, 4 # increment memory address
@@ -198,8 +200,10 @@ mm_print:
   	move $t2, $zero # int j = 0
   	ForP2:
       li $v0, 1
-  		lw $a0, 0($t0)
+      move $t3, $a0
+      lw $a0, 0($t0)
       syscall
+      move $a0, $t3
 
   		addi $t2, $t2, 1 # int j += 1
       addi $t0, $t0, 4 # increment memory address
@@ -224,12 +228,113 @@ mm_print:
 #   v0 = base address for result matrix
 ################################################################################
 mm_mult:
+
+  # #print out values to show that it is working
+  # li $v0 1
+  # syscall
+  # move $a0 $a1
+  # syscall
+  # move $a0 $a2
+  # syscall
+
+  # move $a0 $a3
+  # syscall
+
+  # move $a0 $sp
+  # syscall
+  
+
   # save return address and any saved registers on the stack, if necessary
+  move $t1, $ra # save return address
 
   # set up and call mm_alloc for result matrix
- 
-  # three nested loops for matrix-matrix multiplication
+  move $t0, $a1 # save $a1 in $t0
+  move $a1, $a3 # borrow a3 to do the call
+  jal mm_alloc #v0 contains the address of the result matrix
+  move $ra, $t1 # move return address back
+  move $a1, $t0 # revert back the values
 
+  # three nested loops for matrix-matrix multiplication
+  move $t1, $zero # int i = 0
+  ForMM1:
+    move $t2, $zero # int j = 0
+    
+    ForMM2:
+      move $t4, $zero # sum of the slot
+      
+      move $t3, $zero # int k = 0
+      ForMM3:
+        # getting mat1->data[i][k]
+        move $t0, $a2 # $t0 contains the base address for matrix 1
+        move $t6, $t1
+        mul $t6, $t6, $a3 # times a3
+        sll $t6, $t6, 2 # times 4
+        add $t0, $t0, $t6 # t0 = $a2 + 4 x $a3 x $t1
+
+        move $t6, $t3
+        sll $t6, $t6, 2 # $t6 = 4 x $t3
+        add $t0, $t0, $t6 # t0 = $a2 + 4 x $a3 x $t1 + 4 x $t3
+        # $s0 now contains the address of data[i][k]
+        move $s0, $t0
+        
+        # getting mat2->data[k][j]
+        move $t0, $sp # t0 contains the base address for matrix 2
+        move $t6, $t3
+        mul $t6, $t6, $a3 # times a3
+        sll $t6, $t6, 2 # times 4
+        add $t0, $t0, $t6
+
+        move $t6, $t2
+        sll $t6, $t6, 2
+        add $t0, $t0, $t6
+        move $s1, $t0 #$s1 contains the address of data[k][j]
+
+        # FUCKING UP HEREEEEEEEEE
+
+        # move $a0, $s0
+        # li $v0, 1
+        # syscall
+
+        # move $a0, $s1
+        # syscall
+        li $v0, 1
+        lw $a0, 0($s0)
+        syscall
+
+        lw $a0, 0($s1)
+        syscall
+
+        li $v0, 10
+        syscall
+
+        # get value stored at $s0 -> store at $t0
+        lw $t0, 0($s0)
+
+        # get value stored at $s1 -> store at $t6
+        lw $t6, 0($s1)
+
+        # multiply the two
+        mul $t0, $t0, $t6
+        add $t4, $t4, $t0 # add the value onto the sum
+
+        blt $t3, $a1, ForMM3
+      # get address of result_matrix->data[i][j]
+      move $t0, $v0 # address for the result_matrix
+      move $t6, $t1
+      mul $t6, $t6, $a3
+      sll $t6, $t6, 2
+      add $t0, $t0, $t6 # data[i]
+
+      move $t6, $t2
+      sll $t6, $t6, 2
+      add $t0, $t0, $t6 # data[i][j]
+
+      sw $t4, 0($t0) # data[i][j] = sum
+      addi $t2, $t2, 1
+      blt $t2, $a3, ForMM2
+
+    addi $t1, $t1, 1
+    blt $t1, $a0, ForMM1
   # set return value
 
   # restore stack, ra, and any saved registers, if necessary
