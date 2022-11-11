1e265105c7dab555c98df73e52b52da85e8f4ba4
Ardennt <jiaweiw02@gmail.com>
2022-11-11 03:08:36 -0500

    hw5

--- homework/Homework_05/main.c
+++ homework/Homework_05/main.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// function returns inputarr as a list of chars
 int parse_input(char* line, char* inputarr[]){
     int count = 0;
     
     char var[8];
     while (sscanf(line, "%c", &var)){
-        
+        inputarr[count++] = var;
     }
 }
 
-int main(int argc, char* argv[]){
-    // argv[1] will be the filename
-    FILE *in_file = fopen(argv[1], "r");
-    if (!in_file) {
-        printf("file not opened successfully");
-        return 1;
-    }
+void translate(char** line_c, FILE* outfile){
+
+}
+
+void read(char* fname){
+    FILE *in_file = fopen(fname, "r");
     
     // allocate 128 spaces for the line to be read
     char line[128];
 
     // read in line by line
     while (fgets(line, sizeof(line), in_file)){
-        
+        // store the line parsed here
+        char parsed[32][8];
+        int total_count = parse_input(line, parsed);
     }
+}
+
+int main(int argc, char* argv[]){
+    // argv[1] will be the filename
+    char in_fname[128] = argv[1];
+    read(in_fname);
 }
