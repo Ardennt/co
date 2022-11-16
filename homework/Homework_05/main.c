ef38d45841042b7815f4371a8cd72264728a4f8a
Ardennt <jiaweiw02@gmail.com>
2022-11-15 19:14:26 -0500

    homework5

--- homework/Homework_05/main.c
+++ homework/Homework_05/main.c
@@ -7,7 +7,6 @@
 struct reg {
     char letter;
     int reg_num;
-    int val;
 };
 
 int inReg(struct reg registers[10], char letter){
@@ -20,54 +19,150 @@ int inReg(struct reg registers[10], char letter){
 }
 
 // returns the value being loaded into
-int use_LI(struct reg registers[10], int ourReg, char tmp[8]){
+void use_LI(struct reg registers[10], int ourReg, char tmp[8]){
     tmp[strlen(tmp)-1] = '\0';
     int val;
     // all variables are single letters, we can make this assumption
-    // DON'T KNOW IF WE NEED THIS??????
-    if (isalpha(tmp[0])){
+    if (isalpha(tmp[0])){ // might not need this? not stated in assumption if a = b is possible
         // we assume that all variables on the rhs should be in registers already
         int varReg = inReg(registers, tmp[0]);
-        val = registers[varReg].val;
-        printf("add $s%d, $s%d, $%d", ourReg, ourReg, varReg);
-        return val;
+        printf("add $s%d,$s%d,$%d", ourReg, ourReg, varReg);
+        // return val;
     } else {
         val = atoi(tmp);
-        printf("li $s%d, %d\n", ourReg, val);
-        return val;
+        printf("li $s%d,%d\n", ourReg, val);
     }
 }
 
+void remove_semicolon(char var[8]){
+    var[strlen(var)-1] = '\0';
+}
+
+void plus_minus(struct reg registers[10], int ourReg, char var1[8], char var2[8], int tmp_var, int usedtmp, char operation){
+    // var1 is the first variable and var2 is the second variable
+    // int t_count = 0;
+    int first_var = inReg(registers, var1[0]);
+    int second_var = inReg(registers, var2[0]);
+
+    // if var2 is the last variable, we can just add tmp_var to 
+    // our var along with the last variable
+    if (var2[strlen(var2)-1] == ';'){
+        // add/sub condition
+        if (isalpha(var2[0])){
+            // used a temporary variable
+            if (operation == '+') printf("add ");
+            else printf("sub ");
+
+            if (usedtmp) {
+                printf("$s%d,$t%d,$s%d\n", ourReg, tmp_var, second_var);
+            // didn't use a temporary variable
+            } else {
+                printf("$s%d,$s%d,$s%d\n", ourReg, first_var, second_var);
+            }
+        }
+        // addi condition
+        else {
+            // used a temporary variable
+            char new_var2[8]; // seg fault, a new variable is needed
+            sscanf(var2, "%s", new_var2);
+            remove_semicolon(new_var2);
+            if (usedtmp) {
+                printf("addi $s%d,$t%d,", ourReg, tmp_var);
+            }
+            else {
+                printf("addi $s%d,$s%d,", ourReg, first_var);
+            }
+
+            if (operation == '-') printf("-%s\n", new_var2);
+            else printf("%s\n", new_var2);
+            
+        }
+        return;
+    }
+
+    // var2 isn't the last variable
+    // add condition
+    if (isalpha(var2[0])){
+        if (operation == '+') printf("add ");
+        else printf("sub ");
+
+        if (usedtmp) {
+            printf("$t%d,$t%d,$s%d\n", tmp_var, tmp_var-1, second_var);
+        } else {
+            printf("$t%d,$s%d,$s%d\n", tmp_var, first_var, second_var);
+        }
+    }
+    // addi condition
+    else {
+        if (usedtmp) {
+            printf("addi $t%d,$t%d,", tmp_var, tmp_var-1);
+        }
+        else {
+            printf("addi $t%d,$s%d,", tmp_var, first_var);
+        }
+
+        if (operation == '-') printf("-%s\n", var2);
+        else printf("%s\n", var2);
+    }
+}
+
+void mult_divide(struct reg registers[10], int ourReg, char var1[8], char var2[8], int tmp_var, int usedtmp, char operation){
+    int first_var = inReg(registers, var1[0]);
+    int second_var = inReg(registers, var2[0]);
+
+    
+}
+
 // return the value of the right hand side of the equation
-int getRHS(struct reg registers[10], int ourReg, char line[128], int offset){
+void getRHS(struct reg registers[10], int ourReg, char line[128], int offset, int *tmpReg){
     
     // contains each variable on the right hand side
     char tmp[8];
     // offset counter
     int n = 0;
     // temp register counter
-    int tmpReg = 0;
+    int usedtmp = 0;
     // loading condition (only 1 variable is in RHS)
-    int LI = 0;
+    sscanf(line + offset, "%s%n", tmp, &n);
+    if (tmp[strlen(tmp)-1] == ';'){
+        use_LI(registers, ourReg, tmp);
+        return;
+    }
+    offset += n;
+    
+    // not loading contion:
     while (tmp[strlen(tmp)-1] != ';'){
-        sscanf(line + offset, "%s%n", tmp, &n);
-        // loading condition
-        if (LI == 0 && tmp[strlen(tmp)-1] == ';'){
-            return use_LI(registers, ourReg, tmp);
-        }
 
         // not loading, use operations
+        char operation[2];
+        char var[8];
+        sscanf(line + offset, "%s%s%n", operation, var, &n);
 
-        LI = 1;
+        // if it's the last variable we don't need another tmp holder
+        if (usedtmp && var[strlen(var)-1] != ';'){
+            *tmpReg += 1;
+        }
+        // add and subtraction
+        if (operation[0] == '+' || operation[0] == '-'){
+            plus_minus(registers, ourReg, tmp, var, *tmpReg, usedtmp, operation[0]);
+        } 
+        else if (operation[0] == '*' || operation[0] == '/'){
+
+        }
+
         offset += n;
+        // used a temp variable
+        
+        usedtmp = 1;
+        // put var in tmp
+        sscanf(var, "%s", tmp);
     }
-    return 1;
 }
 
 // helper function to see all the variables
 void printValues(struct reg registers[10]){
     for (int i = 0; i < 10; i++){
-        printf("variable: %c, registers: $s%d, value: %d\n", registers[i].letter, registers[i].reg_num, registers[i].val);
+        printf("variable: %c, registers: $s%d\n", registers[i].letter, registers[i].reg_num);
     }
 }
 
@@ -80,6 +175,7 @@ void read(char* fname){
     struct reg registers[10];
 
     int reg_count = 0;
+    int tmp_reg = 0;
     // read in line by line
     while (fgets(line, sizeof(line), in_file)){
         printf("# %s", line);
@@ -89,7 +185,6 @@ void read(char* fname){
         }
         // name of variable
         char var;
-        char equal; // useless variable
         int offset = 0;
         
         char tmp[8];
@@ -106,17 +201,12 @@ void read(char* fname){
             // assign reg_count and var
             r.letter = var;
             r.reg_num = reg_count;
-            // get value
-            r.val = getRHS(registers, reg_count, line, offset);
-            // add to registers
-            registers[reg_count++] = r;
-        }
-        // if the variable on the left side already exists
-        else {
-            registers[regnum].val = getRHS(registers, regnum, line, offset);
+            registers[reg_count] = r;
+            getRHS(registers, reg_count, line, offset, &tmp_reg);
+            reg_count += 1;
         }
     }
-    printValues(registers);
+    // printValues(registers);
 }
 
 int main(int argc, char* argv[]){
