670e6a664df744b1de9eacc67c7c20fec683fca3
Ardennt <jiaweiw02@gmail.com>
2022-11-30 00:21:25 -0500

    hw6 formatting errors

--- homework/Homework_06/main.c
+++ homework/Homework_06/main.c
@@ -8,11 +8,6 @@ struct var{
     int line; // tracks what line it was last updated
 };
 
-// this function updates a variable with the line they last appeared in
-void update(struct var v, struct var v_arr[16], int* vcount) {
-
-}
-
 int getIndex(char *v, struct var v_arr[16]) {
     for (int i = 0; i < 16; i++){
         if (v_arr[i].regType == v[1] && v_arr[i].regNum == v[2])
@@ -21,59 +16,170 @@ int getIndex(char *v, struct var v_arr[16]) {
     return -1;
 }
 
-// this function checks if the variables are dependencies or not
-void depen(char line[64], int lineN, struct var v_arr[16]) {
+void parse(char line[64], char reg[8], char v1[8], char v2[8]){
     char instruction[8];
     char variables[16];
     sscanf(line, "%s%s", instruction, variables);
-    // lw and sw conditions are different...
-    if (instruction[1] == 'w') return;
-     
-    // parse variables, split by comma
-    char *ptr; // temporary pointer
-    char *reg; // our register
-    char *v1;  // first register
-    char *v2;  // second register 
 
-    ptr = strtok(variables, ",");
-    reg = ptr;
-    ptr = strtok(NULL, ",");
-    v1 = ptr;
-    ptr = strtok(NULL, ",");
-    v2 = ptr;
+    // parse variables
+    char *ptr = strtok(variables, ",");
+    for (int i = 0; i < strlen(ptr); i++){
+        reg[i] = ptr[i];
+    }
+    reg[strlen(ptr)] = '\0';
 
-    // check if v1 and v2 are dependencies
-    int v1_ind = getIndex(v1, v_arr);
-    int v2_ind = getIndex(v2, v_arr);
+    // if instructions are sw or lw:
+    if (instruction[1] == 'w') {
+        ptr = strtok(NULL, "(");
+        for (int i = 0; i < strlen(ptr); i++){
+            v2[i] = ptr[i];
+        }
+        v2[strlen(ptr)] = '\0';
 
-    // continue here: check if v2 is a constant, if it is, you can ignore checking it for dependency
+        ptr = strtok(NULL, ")");
+        for (int i = 0; i < strlen(ptr); i++){
+            v1[i] = ptr[i];
+        }
+        v1[strlen(ptr)] = '\0';
+
+    } else {
+        ptr = strtok(NULL, ",");
+        for (int i = 0; i < strlen(ptr); i++){
+            v1[i] = ptr[i];
+        }
+        v1[strlen(ptr)] = '\0';
+
+        ptr = strtok(NULL, ",");
+        for (int i = 0; i < strlen(ptr); i++){
+            v2[i] = ptr[i];
+        }
+        v2[strlen(ptr)] = '\0';
+    }
 }
 
-void read(char fname[128], struct var v_arr[16]){
+// this function updates a variable with the line they last appeared in
+void update(char line[64], struct var v_arr[16], int* vcount, int lineN) {
+    char reg[8];
+    char v1[8];
+    char v2[8];
+    parse(line, reg, v1, v2);
+
+    int ind = getIndex(reg, v_arr);
+    if (ind == -1) {
+        // add it into v_arr
+        struct var tmp;
+        tmp.line = lineN;
+        tmp.regType = reg[1];
+        tmp.regNum = reg[2];
+        v_arr[*vcount] = tmp;
+        *vcount += 1;
+    } else {
+        v_arr[ind].line = lineN;
+    }
+}
+
+// this function checks if the variables are dependencies or not
+// returns the numbers of nops required
+int depen(char line[64], int lineN, struct var v_arr[16]) {
+    char reg[8];
+    char v1[8];
+    char v2[8];
+    parse(line, reg, v1, v2);
+     
+    // check if v1 and v2 are dependencies
+    int v1_ind = getIndex(v1, v_arr);
+    // calculates the difference of lines in between me and the index
+    int v1dif = -1;
+    
+    if (v1_ind != -1) {
+        // printf("lineN: %d, %c%c line: %d\n", lineN, v_arr[v1_ind].regType, v_arr[v1_ind].regNum, v_arr[v1_ind].line);
+        if (v1_ind != -1) v1dif = lineN - v_arr[v1_ind].line - 1;
+    }
+    
+    // check if v2 is a constant, if it is, you can ignore checking it for dependency
+    int v2dif = -1;
+    
+    if (v2[0] == '$'){
+        int v2_ind = getIndex(v2, v_arr);
+        if (v2_ind != -1) v2dif = lineN - v_arr[v2_ind].line - 1;
+    }
+
+    // printf("v1dif: %d v2dif: %d\n", v1dif, v2dif);
+    if (v1dif == 0 || v2dif == 0) return 2;
+    if (v1dif == 1 || v2dif == 1) return 1;
+    return 0;
+}
+
+int read(char fname[128], struct var v_arr[16], int cycle, char stages[][4]){
     // declare the file
     FILE* file = fopen(fname, "r");
     // line buffer
     char line[64];
+    // variable counter;
+    int v_count = 0;
     // read line by line
-    for (int lineN = 1; fgets(line, sizeof(line), file); lineN++){
+    int lineN = 1;
+    for (; fgets(line, sizeof(line), file); lineN++){
         /*
         step for each line:
         1. check if the registers being read are dependencies
         2. store myself into the array of variables
         3. increment everyone's steps
         */
+        // check if the registers being read are dependencies
+        int dep = depen(line, lineN, v_arr);
+        for (int i = 0; i < dep; i++){
+            printf("nop\t");
+            for (int i = 1; i <= 9; i++){
+                if (i < lineN || i > cycle || lineN + 4 < i){
+                    printf("\t.");
+                    continue;
+                }
+                else if (i > lineN + 1) {
+                    printf("\t*");
+                    continue;
+                }
+                printf("\t%s", stages[i - lineN]);
+            }
+            printf("\n");
+        }
+        lineN += dep;
 
-       // check if the registers being read are dependencies
-       depen(line, lineN, v_arr);
+        strtok(line, "\n");
+        printf("%s", line);
+
+        for (int i = 1; i <= 9; i++){
+            if (i < lineN || i > cycle || lineN + 4 < i){
+                printf("\t.");
+                continue;
+            }
+            printf("\t%s", stages[i - lineN]);
+        }
+        printf("\n");
+
+        // printf("\t.\t.\t.\t.\t.\t.\t.\t.\t.\n");
+        // store myself into the array of variables
+        update(line, v_arr, &v_count, lineN);
     }
+    lineN -= 1;
+    if (lineN + 4 == cycle) return 1;
+    return 0;
 }
 
-void print() {
+void print(char fname[128], struct var v_arr[16], char stages[][4]) {
+    printf("START OF SIMULATION\n\n");
+    
+    for (int i = 1; i <= 9; i++) {
+        printf("CPU Cycles ===>\t1\t2\t3\t4\t5\t6\t7\t8\t9\n");
+        if (read(fname, v_arr, i, stages) == 1) break;
+        printf("\n");
+    }
 
+    printf("\nEND OF SIMULATION\n");
 }
 
 int main(int argc, char* argv[]) {
-    // char stages[][4] = {"IF", "ID", "EX", "MEM", "WB"};
+    char stages[][4] = {"IF", "ID", "EX", "MEM", "WB"};
     struct var v_arr[16];
-    read(argv[1], v_arr);
+    print(argv[1], v_arr, stages);
 }
